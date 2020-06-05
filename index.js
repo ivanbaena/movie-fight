6c1280f0f5067d01806c2b507a1996f2a91f52e8
Ivan Baena <baenadev.io@gmail.com>
2020-06-04 22:58:32 -0400

    autocomplete refactor complete

--- index.js
+++ index.js
@@ -1,57 +1,78 @@
 const API_KEY = '6c32d55d';
 
-const fetchData = async (searchTerm) => {
+createAutoComplete({
+  root: document.querySelector('.autocomplete'),
+  renderOption(movie) {
+    const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
+    return `
+        <img src="${imgSrc}" />
+        ${movie.Title} (${movie.Year})
+      `;
+  },
+  onOptionSelect(movie) {
+    onMovieSelect(movie);
+  },
+  inputValue(movie) {
+    return movie.Title;
+  },
+  async fetchData(searchTerm) {
+    const res = await axios.get('http://www.omdbapi.com/', {
+      params: {
+        apikey: API_KEY,
+        s: searchTerm,
+      },
+    });
+    if (res.data.Error) return [];
+    return res.data.Search;
+  },
+});
+
+const onMovieSelect = async (movie) => {
   const res = await axios.get('http://www.omdbapi.com/', {
     params: {
       apikey: API_KEY,
-      s: searchTerm,
+      i: movie.imdbID,
     },
   });
 
-  if (res.data.Error) return [];
-
-  return res.data.Search;
+  document.querySelector('#summary').innerHTML = movieTemplate(res.data);
 };
 
-const root = document.querySelector('.autocomplete');
-root.innerHTML = `
-    <label><b>Search For a Movie</b></label>
-        <input class="input"/>
-        <div class="dropdown">
-          <div class="dropdown-menu">
-            <div class="dropdown-content results"></div>
-          </div>
-        </div>
-`;
-const input = document.querySelector('input');
-const dropdown = document.querySelector('.dropdown');
-const resultsWrapper = document.querySelector('.results');
-
-const onInput = async (event) => {
-  const movies = await fetchData(event.target.value);
-
-  if (!movies.length) {
-    dropdown.classList.remove('is-active');
-    return;
-  }
-  resultsWrapper.innerHTML = '';
-  dropdown.classList.add('is-active');
-  for (let movie of movies) {
-    const option = document.createElement('a');
-    const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
-    option.classList.add('dropdown-item');
-    option.innerHTML = `
-        <img src="${imgSrc}" />
-        ${movie.Title}
-      `;
-
-    resultsWrapper.appendChild(option);
-  }
+const movieTemplate = (movieDetail) => {
+  return `
+        <article class="media">
+            <figure class="media-left">
+                <p class="image">
+                    <img src="${movieDetail.Poster}" />
+                </p>
+            </figure>
+            <div class="media-content">
+                <div class="content">
+                    <h1>${movieDetail.Title}</h1>
+                    <h4>${movieDetail.Genre}</h4>
+                    <p>${movieDetail.Plot}</p>
+                </div>
+            </div>
+        </article>
+        <article class="notification is-primary">
+            <p class="title">${movieDetail.Awards}</p>
+            <p class="subtitle">Awards</p>
+        </article>
+        <article class="notification is-primary">
+            <p class="title">${movieDetail.BoxOffice}</p>
+            <p class="subtitle">Box Office</p>
+        </article>
+        <article class="notification is-primary">
+            <p class="title">${movieDetail.Metascore}</p>
+            <p class="subtitle">Metascore</p>
+        </article>
+        <article class="notification is-primary">
+            <p class="title">${movieDetail.imdbRating}</p>
+            <p class="subtitle">IMDB Rating</p>
+        </article>
+                <article class="notification is-primary">
+            <p class="title">${movieDetail.imdbVotes}</p>
+            <p class="subtitle">IMDB Votes</p>
+        </article>
+    `;
 };
-input.addEventListener('input', debounce(onInput, 1000));
-
-document.addEventListener('click', (event) => {
-  if (!root.contains(event.target)) {
-    dropdown.classList.remove('is-active');
-  }
-});
